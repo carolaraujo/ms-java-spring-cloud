fdd4867256aac56bc6c10b7453de3a55e9a7719e
Caroline Araujo <47358325+carolaraujo@users.noreply.github.com>
2021-08-24 16:56:42 -0300

    Initial commit

---
+++ src/main/java/com/example/msjavaspring/cloud/MsJavaSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.msjavaspring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJavaSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsJavaSpringCloudApplication.class, args);
+	}
+
+}
